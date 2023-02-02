6e9087485e40969512f74fc7a268f002360478ac
Fredy Polania <fpolania@gmail.com>
2023-02-02 09:56:49 -0400

    add: netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import WindiCSS from "vite-plugin-windicss";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [WindiCSS()],
+    plugins: [WindiCSS()]
   },
-
   integrations: [WindiCSS(), image()],
+  output: "server",
+  adapter: netlify()
 });
